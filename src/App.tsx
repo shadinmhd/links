42c76510c309852881112e6d5acaf39971c7c7f7
shadinmhd <108604275+shadinmhd@users.noreply.github.com>
2023-10-15 21:04:20 +0530

    added whatsapp link

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
-import { useEffect } from "react"
 import profileImage from "./assets/profile.png"
-import { useAnimate } from "framer-motion"
 import Link from "./components/Link"
 import { faEarth } from "@fortawesome/free-solid-svg-icons/faEarth"
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons"
 import { faInstagram } from "@fortawesome/free-brands-svg-icons/faInstagram"
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons/faWhatsapp"
 
 const App = () => {
-	// @ts-ignore
-	const [scope, animate] = useAnimate()
-
-	useEffect(() => {
-
-	}, [])
 
 	return (
 		<div className="flex flex-col gap-10 items-center justify-center h-screen w-screen text-white bg-[#101010]">
-			<div className="flex flex-col items-center justify-center ">
+			<div className="flex flex-col gap-4 items-center justify-center ">
 				<div className="rounded-full">
 					<img className="h-40 w-40 bg-black rounded-full" src={profileImage} alt="dp" />
 				</div>
@@ -25,16 +18,18 @@ const App = () => {
 				</div>
 			</div>
 			<div
-				ref={scope}
 				className="flex flex-col gap-5"
 			>
-				<Link icon={faEarth} to="https://shadinmhd.in" >
+				<Link index={1} icon={faEarth} to="https://shadinmhd.in" >
 					Website
 				</Link>
-				<Link icon={faLinkedin} to="https://www.linkedin.com/in/shadin-muhammed-69b004256/" >
+				<Link index={2} icon={faLinkedin} to="https://www.linkedin.com/in/shadin-muhammed-69b004256/" >
 					Linkedin
 				</Link>
-				<Link icon={faInstagram} to="https://www.instagram.com/_sh.dn_/" >
+				<Link index={3} icon={faInstagram} to="https://www.instagram.com/_sh.dn_/" >
+					Instagram
+				</Link>
+				<Link index={3} icon={faWhatsapp} to="https://wa.me/+918589908074" >
 					Instagram
 				</Link>
 
