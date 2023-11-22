d02bcc69952449c81b6cd15b72936facc69ea956
Shadin Muhammed <108604275+shadinmhd@users.noreply.github.com>
2023-11-22 21:54:44 +0530

    using vite

--- README.md
+++
@@ -1,3 +0,0 @@
-# links
-
-> this is a simple site i made for keeping all my contact/social meda links in a singel place
--- script.js
+++
@@ -1,9 +0,0 @@
-const link = document.getElementById(".link")
-
-gsap.to(link,{
-	opacity : 0,
-	x : -50,
-	duration : 1,
-	stagger : 0.5,
-	ease : "power2.out",
-})
